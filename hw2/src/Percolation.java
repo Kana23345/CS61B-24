18df815b9ea3559c7a140dbfc2da9f68705cb17c
kana <503545466@qq.com>
2025-08-26 17:35:21 +0800

    finished HW2

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -3,33 +3,69 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     // TODO: Add any necessary instance variables.
+    private boolean[][] grid;
+    private int openSites = 0;
+    private WeightedQuickUnionUF mainUfSet;
+    private WeightedQuickUnionUF helpUfSet;
+    private int TVIndex;
+    private int BVIndex;
+    private int size;
 
     public Percolation(int N) {
         // TODO: Fill in this constructor.
+        grid = new boolean[N][N];
+        size = N;
+        mainUfSet = new WeightedQuickUnionUF(N * N + 2);
+        helpUfSet = new WeightedQuickUnionUF(N * N + 1);
+        TVIndex = N * N;
+        BVIndex = N * N + 1;
+    }
+
+    private int xyTo1D(int row, int col) {
+        return row * size + col;
+    }
+
+    private void bigUnion(int index, int other) {
+        mainUfSet.union(index, other);
+        helpUfSet.union(index, other);
     }
 
     public void open(int row, int col) {
         // TODO: Fill in this method.
+        if (!grid[row][col]) {
+            openSites += 1;
+            grid[row][col] = true;
+        }
+        int index = xyTo1D(row, col);
+        if (row > 0 && isOpen(row - 1, col)) bigUnion(index, xyTo1D(row - 1, col));
+        if (col > 0 && isOpen(row, col - 1)) bigUnion(index, xyTo1D(row, col - 1));
+        if (row < size && isOpen(row + 1, col)) bigUnion(index, xyTo1D(row + 1, col));
+        if (col < size && isOpen(row, col + 1)) bigUnion(index, xyTo1D(row, col + 1));
+        if (row == 0) bigUnion(index, TVIndex);
+        if (row == size - 1) mainUfSet.union(index, BVIndex);
+
     }
 
     public boolean isOpen(int row, int col) {
         // TODO: Fill in this method.
-        return false;
+        if (!(row < size && row >= 0)) return false;
+        if (!(col < size && col >= 0)) return false;
+        return grid[row][col];
     }
 
     public boolean isFull(int row, int col) {
         // TODO: Fill in this method.
-        return false;
+        return helpUfSet.connected(xyTo1D(row, col), TVIndex);
     }
 
     public int numberOfOpenSites() {
         // TODO: Fill in this method.
-        return 0;
+        return openSites;
     }
 
     public boolean percolates() {
         // TODO: Fill in this method.
-        return false;
+        return mainUfSet.connected(TVIndex, BVIndex);
     }
 
     // TODO: Add any useful helper methods (we highly recommend this!).
--- hw2/tests/PercolationTest.java
+++ hw2/tests/PercolationTest.java
@@ -13,7 +13,7 @@ public class PercolationTest {
      * (1) OPEN: isOpen() returns true, isFull() returns false
      * <p>
      * (2) INVALID: isOpen() returns false, isFull() returns true
-     *              (This should not happen! Only open cells should be full.)
+     * (This should not happen! Only open cells should be full.)
      * <p>
      * (3) FULL: isOpen() returns true, isFull() returns true
      * <p>
@@ -82,7 +82,27 @@ public class PercolationTest {
     //       write some more tests and delete the fail() line
     @Test
     public void yourFirstTestHere() {
-        fail("Did you write your own tests?");
-    }
+        int N = 5;
+        Percolation p = new Percolation(5);
+        p.open(2, 2);
+        p.open(2, 3);
+        p.open(3, 3);
+        p.open(1, 2);
+        p.open(0, 2);
+        assertThat(p.percolates()).isFalse();
+        p.open(4, 3);
+        assertThat(p.percolates()).isTrue();
+        p.open(4, 0);
+        p.open(3, 0);
+        assertThat(p.isFull(3, 0)).isFalse();
+        Cell[][] expectedState = {
+                {Cell.CLOSED, Cell.CLOSED, Cell.FULL, Cell.CLOSED, Cell.CLOSED},
+                {Cell.CLOSED, Cell.CLOSED, Cell.FULL, Cell.CLOSED, Cell.CLOSED},
+                {Cell.CLOSED, Cell.CLOSED, Cell.FULL, Cell.FULL, Cell.CLOSED},
+                {Cell.OPEN, Cell.CLOSED, Cell.CLOSED, Cell.FULL, Cell.CLOSED},
+                {Cell.OPEN, Cell.CLOSED, Cell.CLOSED, Cell.FULL, Cell.CLOSED}
+        };
+        assertThat(getState(N, p)).isEqualTo(expectedState);
 
+    }
 }
